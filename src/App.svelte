dbd9d1af1a72dfa6229da2fd8a971fbae2ef3788
gamzamandu <galbimandu1126@gmail.com>
2025-02-02 00:23:23 +0900

    set up :: vite + svelte5 + scss + svelte-spa-router

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Router from 'svelte-spa-router';
+  import Index from './routes/Index.svelte';
+
+  const routes = {
+    '/': Index,
+  };
+</script>
+
+
+<Router {routes} />
---
+++ src/routes/Index.svelte
@@ -0,0 +1,7 @@
+<!-- Index.svelte -->
+<script>
+  import '../styles/variables.scss';
+</script>
+
+
+<h1 class="text-color">ㅁㅇㄴㄹ</h1>
