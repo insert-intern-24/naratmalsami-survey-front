130dc411f46478d8ec0adf1e3e2193823d23adc3
gamzamandu <galbimandu1126@gmail.com>
2025-02-03 20:55:26 +0900

    add : index page 완성 및 트랜지션 추가

--- src/routes/Index.svelte
+++ src/routes/Index.svelte
@@ -1,11 +1,56 @@
-<!-- Index.svelte -->
 <script>
+  import { fly } from 'svelte/transition';
+  import { backOut } from 'svelte/easing';
+  
   import '../styles/variables.scss';
+
+  let text_number = 0;
+  
+  const texts = [
+    '안녕하세요 반가워요!',
+    '저희는 납득 가능한\n외래어 순화AI를 제작하고 있어요',
+    'AI를 학습시키기 위해서는\n많은 데이터가 필요해요',
+    '여러분의 도움이 필요해요!',
+    '다음 페이지부터\n 국립국어원에서 제시한\n 다듬은 말에 대한 만족도를\n 선택해주시면 되요'
+  ];
+
+  function changeText() {
+    if (text_number < texts.length - 1){
+      text_number = (text_number + 1) % texts.length;
+    }
+    else {
+      window.location.href = '/survey';
+    }
+  };
+
+  function translateText(text) {
+    return text
+      .replace(/\n/g, "<br/>")
+      .replace(/국립국어원/g, '<img alt="국립국어원" src="src/assets/국립국어원.png" style="margin-right: 10px; margin-bottom: -13px"/>');
+  };
+
 </script>
 
-
-<main>
-  <h1 class="text-color">안녕하세요 반가워요!</h1>
+<main on:click={changeText}>
+  {#key text_number}
+    <div
+      class="text-color"
+      in:fly={{
+        y: 100,
+        delay: 200,
+        easing: backOut,
+        duration: 700
+      }}
+      out:fly={{
+        y: -100, 
+        delay: 200,
+        easing: backOut, 
+        duration: 700
+      }}
+    >
+      {@html translateText(texts[text_number])}
+    </div>
+  {/key}
 </main>
 
 <style lang="scss">
@@ -20,9 +65,17 @@
     display: flex;
     justify-content: center;
     align-items: center;
+    position: relative; 
 
-    h1 {
+    div {
+      user-select: none;
+      cursor: pointer;
       font-size: 6rem;
+      color: #fff;
+      text-align: left;
+      position: absolute;
+      font-weight: bold;
     }
   }
+
 </style>
