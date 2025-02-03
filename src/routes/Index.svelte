678958087fb52588c31684d658be956278c54fb6
gamzamandu <galbimandu1126@gmail.com>
2025-02-03 19:19:22 +0900

    add : background에 이미지 삽입, 텍스트 정렬

--- src/routes/Index.svelte
+++ src/routes/Index.svelte
@@ -4,4 +4,25 @@
 </script>
 
 
-<h1 class="text-color">ㅁㅇㄴㄹ</h1>
+<main>
+  <h1 class="text-color">안녕하세요 반가워요!</h1>
+</main>
+
+<style lang="scss">
+  main {
+    min-height: 100dvh;
+    min-width: 100dvh;
+    background-size: cover;
+    background-image: url('../assets/background.png');
+    background-attachment: fixed;
+    overflow: hidden;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    h1 {
+      font-size: 6rem;
+    }
+  }
+</style>
