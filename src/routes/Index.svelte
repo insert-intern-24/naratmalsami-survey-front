b79d8312330ed6f7abcddad82cb97d8f1b0b73e6
gamzamandu <galbimandu1126@gmail.com>
2025-02-06 01:40:52 +0900

    add : .prettierrc 설정 및 적용

--- src/routes/Index.svelte
+++ src/routes/Index.svelte
@@ -1,65 +1,70 @@
 <script>
-	import { fly } from 'svelte/transition';
-	import { backOut } from 'svelte/easing';
-	import { push } from 'svelte-spa-router';
+  import { fly } from "svelte/transition";
+  import { backOut } from "svelte/easing";
+  import { push } from "svelte-spa-router";
 
-	let text_number = 0;
+  let text_number = 0;
 
-	const texts = [
-		'클릭으로 넘어가요!\n',
-		'안녕하세요 반가워요!\n',
-		'저희는 납득 가능한\n외래어 순화AI를 제작하고 있어요',
-		'AI를 학습시키기 위해서는\n많은 데이터가 필요해요',
-		'여러분의 도움이 필요해요!',
-		'다음 페이지부터\n 국립국어원에서 제시한\n 다듬은 말에 대한 만족도를\n 선택해주시면 되요',
-	];
+  const texts = [
+    "클릭으로 넘어가요!\n",
+    "안녕하세요 반가워요!\n",
+    "저희는 납득 가능한\n외래어 순화AI를 제작하고 있어요",
+    "AI를 학습시키기 위해서는\n많은 데이터가 필요해요",
+    "여러분의 도움이 필요해요!",
+    "다음 페이지부터\n 국립국어원에서 제시한\n 다듬은 말에 대한 만족도를\n 선택해주시면 되요"
+  ];
 
-	function changeText() {
-		if (text_number < texts.length - 1) {
-			text_number = (text_number + 1) % texts.length;
-		} else {
-			push('/survey');
-		}
-	}
+  function changeText() {
+    if (text_number < texts.length - 1) {
+      text_number = (text_number + 1) % texts.length;
+    } else {
+      push("/survey");
+    }
+  }
 
-	function translateText(text) {
-		return text.replace(/\n/g, '<br/>').replace(/국립국어원/g, '<img alt="국립국어원" src="src/assets/국립국어원.png" style="margin-right: 10px; margin-bottom: -13px"/>');
-	}
+  function translateText(text) {
+    return text
+      .replace(/\n/g, "<br/>")
+      .replace(
+        /국립국어원/g,
+        '<img alt="국립국어원" src="src/assets/국립국어원.png" style="margin-right: 10px; margin-bottom: -13px"/>'
+      );
+  }
 </script>
 
 <main on:click={changeText}>
-	{#key text_number}
-		<div
-			in:fly={{
-				y: 100,
-				delay: 200,
-				easing: backOut,
-				duration: 700,
-			}}
-			out:fly={{
-				y: -100,
-				delay: 200,
-				easing: backOut,
-				duration: 700,
-			}}
-		>
-			{@html translateText(texts[text_number])}
-		</div>
-	{/key}
+  {#key text_number}
+    <div
+      in:fly={{
+        y: 100,
+        delay: 200,
+        easing: backOut,
+        duration: 700
+      }}
+      out:fly={{
+        y: -100,
+        delay: 200,
+        easing: backOut,
+        duration: 700
+      }}
+    >
+      {@html translateText(texts[text_number])}
+    </div>
+  {/key}
 </main>
 
 <style lang="scss">
-	@use '../styles/_mixins.scss' as mixins;
-	main {
-		@include mixins.background('dark');
+  @use "../styles/_mixins.scss" as mixins;
+  main {
+    @include mixins.background("dark");
 
-		div {
-			@include mixins.text('dark');
-			font-size: 6rem;
-			user-select: none;
-			text-align: left;
-			position: absolute;
-			cursor: pointer;
-		}
-	}
+    div {
+      @include mixins.text("dark");
+      font-size: 6rem;
+      user-select: none;
+      text-align: left;
+      position: absolute;
+      cursor: pointer;
+    }
+  }
 </style>
--- src/routes/Survey.svelte
+++ src/routes/Survey.svelte
@@ -1,87 +1,90 @@
 <main>
-	<div>
-		<header>5개의 질문이 남았어요</header>
-		<div>
-			<h1><span>템퍼링</span>을<br /><span>사전 접촉 금지 위반</span>으로 다듬었을 때<br />자연스럽다 생각하십니까?</h1>
-			<h2>탬퍼링은 무단으로 변경하거나 조작하는 행위를 의미합니다</h2>
-			<div>
-				<ul>
-					{#each [1, 2, 3, 4, 5] as num}
-						<li on:click={() => handleClick(num)}>{num}</li>
-					{/each}
-				</ul>
-			</div>
-		</div>
-	</div>
+  <div>
+    <header>5개의 질문이 남았어요</header>
+    <div>
+      <h1>
+        <span>템퍼링</span>을<br /><span>사전 접촉 금지 위반</span>으로 다듬었을
+        때<br />자연스럽다 생각하십니까?
+      </h1>
+      <h2>탬퍼링은 무단으로 변경하거나 조작하는 행위를 의미합니다</h2>
+      <div>
+        <ul>
+          {#each [1, 2, 3, 4, 5] as num}
+            <li on:click={() => handleClick(num)}>{num}</li>
+          {/each}
+        </ul>
+      </div>
+    </div>
+  </div>
 </main>
 
 <style lang="scss">
-	@use '../styles/_mixins.scss' as mixins;
-	@use '../styles/_variables.scss' as var;
-	@import '../styles/functions';
+  @use "../styles/_mixins.scss" as mixins;
+  @use "../styles/_variables.scss" as var;
+  @import "../styles/functions";
 
-	main {
-		@include mixins.background('light');
+  main {
+    @include mixins.background("light");
 
-		div {
-			display: flex;
-			gap: 1.5rem;
-			flex-direction: column;
-			align-items: flex-start;
-			header {
-				background-color: var.$primary-color;
-				color: var.$secondary-color;
-				padding: px-to-rem(18) px-to-rem(24);
-				font-size: px-to-rem(32);
-				border-radius: 50px;
-				line-height: px-to-rem(32);
-			}
-			div {
-				h1 {
-					@include mixins.text('light');
-					font-size: px-to-rem(40);
-					margin: 0px;
-					span {
-						color: var.$primary-color;
-					}
-				}
-				h2 {
-					color: var.$description-color;
-					font-weight: 400;
-					margin: 0px;
-					font-size: px-to-rem(24);
-				}
-				div {
-					ul {
-						display: flex;
-						padding: 0px;
-						gap: px-to-rem(18);
-						li {
-							display: flex;
-							width: 60px;
-							height: 60px;
-							padding: 20px;
-							flex-direction: column;
-							justify-content: center;
-							align-items: center;
-							font-size: px-to-rem(20);
-							font-weight: 400;
-							list-style: none;
-							background: white;
-							border-radius: 100%;
-							border: 2px solid var.$primary-color;
-							color: var.$primary-color;
-							transition: 0.3s ease-in-out;
-							cursor: pointer;
-						}
+    div {
+      display: flex;
+      gap: 1.5rem;
+      flex-direction: column;
+      align-items: flex-start;
+      header {
+        background-color: var.$primary-color;
+        color: var.$secondary-color;
+        padding: px-to-rem(18) px-to-rem(24);
+        font-size: px-to-rem(32);
+        border-radius: 50px;
+        line-height: px-to-rem(32);
+      }
+      div {
+        h1 {
+          @include mixins.text("light");
+          font-size: px-to-rem(40);
+          margin: 0px;
+          span {
+            color: var.$primary-color;
+          }
+        }
+        h2 {
+          color: var.$description-color;
+          font-weight: 400;
+          margin: 0px;
+          font-size: px-to-rem(24);
+        }
+        div {
+          ul {
+            display: flex;
+            padding: 0px;
+            gap: px-to-rem(18);
+            li {
+              display: flex;
+              width: 60px;
+              height: 60px;
+              padding: 20px;
+              flex-direction: column;
+              justify-content: center;
+              align-items: center;
+              font-size: px-to-rem(20);
+              font-weight: 400;
+              list-style: none;
+              background: white;
+              border-radius: 100%;
+              border: 2px solid var.$primary-color;
+              color: var.$primary-color;
+              transition: 0.3s ease-in-out;
+              cursor: pointer;
+            }
 
-						li:hover {
-							background-color: var.$primary-color;
-							color: white;
-						}
-					}
-				}
-			}
-		}
-	}
+            li:hover {
+              background-color: var.$primary-color;
+              color: white;
+            }
+          }
+        }
+      }
+    }
+  }
 </style>
