f684af98f8058d17fa978de5a256a8edf6d21eda
gamzamandu <galbimandu1126@gmail.com>
2025-02-14 21:32:54 +0900

    add : resutl routes 추가

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,12 @@
   import Router from 'svelte-spa-router';
   import Index from './routes/Index.svelte';
   import Survey from './routes/Survey.svelte';
+  import Result from './routes/Result.svelte';
 
   const routes = {
     '/': Index,
     '/survey': Survey,
+    '/result': Result
   };
 </script>
 
---
+++ src/routes/Result.svelte
@@ -0,0 +1,21 @@
+<main>
+  <div>
+    끝
+  </div>
+</main>
+
+<style lang="scss">
+  @use "../styles/_mixins.scss" as mixins;
+  main {
+    @include mixins.background("dark");
+
+    div {
+      @include mixins.text("dark");
+      font-size: 6rem;
+      user-select: none;
+      text-align: left;
+      position: absolute;
+      cursor: pointer;
+    }
+  }
+</style>
