81e6e73a12cfc221422a008a3f8c495ac3e4da0c
galbimandu <galbimandu1126@gmail.com>
2025-03-16 17:29:06 +0900

    add : 이벤트 결과 발표 표시

--- src/routes/Index.svelte
+++ src/routes/Index.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { onMount } from "svelte";
   import { fly } from "svelte/transition";
   import { backOut } from "svelte/easing";
   import { push } from "svelte-spa-router";
@@ -27,9 +28,37 @@
       .replace(/\n/g, "<br/>")
       .replace(
         /국립국어원/g,
-        '<img alt="국립국어원" src="src/assets/국립국어원.png" style="margin-right: 10px; margin-bottom: -13px; height: 5.9375rem; width: auto;"/>'
+        '<img alt="국립국어원" src="/국립국어원.png" style="margin-right: 10px; margin-bottom: -13px; height: 5.9375rem; width: auto;"/>'
       );
   }
+
+  onMount(async () => {
+    try {
+      const who = localStorage.getItem("who") || "";
+      if (who === "") {
+        throw new Error("who값이 없습니다.");
+      }
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/ranking/${who}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Cache-Control": "no-cache"
+        }
+      });
+      if (!response.ok) {
+        throw new Error("API 호출 실패");
+      }
+
+      const result = await response.json();
+      if (result.ranking <= 0) {
+        alert("투표자님은 순위권에 들지 못했습니다. 참여해주셔서 감사합니다");
+        return;
+      }
+      alert(`안녕하세요!, 투표자님의 순위는 ${result.ranking}등 입니다.\n투표에 참가해주셔서 감사합니다.\n디스코드 아이디 ${import.meta.env.VITE_DISCORD}으로 인증코드 ${result.code}를 보내주시면 보상을 증정하겠습니다!\n 인증코드 유출 시 보상은 지급되지 않습니다. (3월 22일까지 연락해주세요)`);
+    } catch (err) {
+      console.error(err);
+    }
+  });
 </script>
 
 <main on:click={changeText} aria-hidden="true">
